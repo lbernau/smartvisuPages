d38c43f55cc92c2e89bf7914cc072311abe101bf
Lars Bernau <L.Bernau@db-central.com>
2014-01-20 11:31:08 +0100

    Inital Commit.

---
+++ visu.js
@@ -0,0 +1,28 @@
+/**
+ * refresh view on cache update.
+ */
+function updateSite(event) {
+	window.applicationCache.swapCache();
+	window.location.reload();
+}
+window.applicationCache.addEventListener('updateready',	updateSite, false);
+
+/**
+ * enable screensaver "qlock"
+ * 
+ * idleTimer() takes an optional argument that defines the idle timeout
+ * timeout is in milliseconds; defaults to 30000
+ */
+$(document).on('pageinit', function(event) {
+	if (jQuery().idleTimer && event.target.id !== 'qlock' && navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) != null) {
+		$.idleTimer(120000, document); //2 minutes
+	}
+});
+
+$(document).bind('idle.idleTimer', function() {
+	$.mobile.changePage("index.php?page=qlock/qlock");
+});
+
+$(document).bind('active.idleTimer', function(event) {
+	parent.history.back();
+});
